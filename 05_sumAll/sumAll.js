e0772c5db7fa7da406f505e72968a980ad4635e1
Justin <Solitro.Justin@gmail.com>
2022-06-07 23:13:44 -0400

    Exercise 05 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(x , y) {
+    let result = 0;
+    if (!((typeof x) === "number" ) || !((typeof y) === "number") || x < 0 || y < 0) {
+        result = "ERROR";
+    } else {
+        const maxInput = Math.max(x, y);
+        const minInput = Math.min(x, y);
+        for (let i = minInput; i <= maxInput; i++){
+            result += i;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
