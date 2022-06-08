0949d823d314dc864b3251cd55b86920a871a9f8
Justin <Solitro.Justin@gmail.com>
2022-06-07 22:31:28 -0400

    Exercise 03 complete.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let gnirts = ''
+    for ( let i = string.length; i >=0; i--) {
+        gnirts += string.charAt(i);
+    }
+    return gnirts;
 };
 
 // Do not edit below this line
