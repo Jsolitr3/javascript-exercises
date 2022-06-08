4c0b3f09b228edd9aa15894501021933f6b0bc7b
Justin <Solitro.Justin@gmail.com>
2022-06-07 22:58:20 -0400

    Exercise 04 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(inputArr , ...args) {
+    for (const values of args) {
+        if (inputArr.indexOf( values ) >= 0) {
+            inputArr.splice( inputArr.indexOf( values ) , 1 );
+        }
+    }
+    return inputArr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
