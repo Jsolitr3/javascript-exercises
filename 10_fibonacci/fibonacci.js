8f820d052f3d528668aa8438693e59732c36f030
jsolitr3 <solitro.justin@gmail.com>
2022-06-20 22:22:44 -0400

    completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    x = parseInt(x);
+    if (x > 0) {
+    const fibArr = [1,1];
+    for (let i = 3; i <=x; i++){
+        fibArr.push(fibArr[i-3]+fibArr[i-2])
+    }
+    return fibArr[fibArr.length-1];
+    } else return "OOPS";
 };
 
 // Do not edit below this line
