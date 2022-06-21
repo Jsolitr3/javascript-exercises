476b6ff49996673f1de4032f9b4fbbc809b45ef5
jsolitr3 <solitro.justin@gmail.com>
2022-06-20 22:09:14 -0400

    completed exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (s1) {
+    s1 = s1.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    s1 = s1.replace(/\s+/g, "");
+    s1 = s1.toLowerCase();
+    let s2 = "";
+    for (let i = s1.length-1; i>=0; i--){
+        s2 =  s2.concat(s1.charAt(i));
+    }
+    return (s1 == s2);
 };
 
 // Do not edit below this line
