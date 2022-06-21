4339aaead5164f7e5dd5c8ed407cbf65e562818d
jsolitr3 <solitro.justin@gmail.com>
2022-06-20 22:28:11 -0400

    exercise 11 complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(x) {
+    const titles = x.map(y => y.title);
+    return titles;
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
