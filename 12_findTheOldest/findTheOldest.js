4e549e4ee71de4d5581f9943e2ffd9a6de6f6439
jsolitr3 <solitro.justin@gmail.com>
2022-06-20 22:53:00 -0400

    completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(x) {
+    const nameList = x.sort(function(a, b) {
+        const lastGuy = (a.hasOwnProperty('yearOfDeath') ? a.yearOfDeath : new Date().getFullYear()) - a.yearOfBirth;
+        const nextGuy = (b.hasOwnProperty('yearOfDeath') ? b.yearOfDeath : new Date().getFullYear())  - b.yearOfBirth;
+        return lastGuy > nextGuy ? -1 : 1;
+    })
+    
+    return nameList[0];
 };
 
 // Do not edit below this line
